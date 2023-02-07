5c6f57ce4c4647d970a463197b0a96b189ee1383
tanpm <tanpm@mobiletech.vn>
2023-02-07 09:54:20 +0700

    fix(crash): android crash when reload runtime base on: https://github.com/software-mansion/react-native-reanimated/pull/3469

--- android/src/main/cpp/NativeProxy.cpp
+++ android/src/main/cpp/NativeProxy.cpp
@@ -37,12 +37,7 @@ NativeProxy::NativeProxy(
       scheduler_(scheduler),
       layoutAnimations(std::move(_layoutAnimations)) {}
 
-NativeProxy::~NativeProxy() {
-  runtime_->global().setProperty(
-      *runtime_,
-      jsi::PropNameID::forAscii(*runtime_, "__reanimatedModuleProxy"),
-      jsi::Value::undefined());
-}
+NativeProxy::~NativeProxy() {}
 
 jni::local_ref<NativeProxy::jhybriddata> NativeProxy::initHybrid(
     jni::alias_ref<jhybridobject> jThis,
